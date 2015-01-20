fde0a87ec85a34434761a899c42b214ccc60c450
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-01-20 15:11:19 +0000

    bootstrap/create_user.py: add new options (checking only; specify password on command line).

--- create_user.py
+++ create_user.py
@@ -9,9 +9,14 @@ import nebulizer
 
 if __name__ == "__main__":
     # Collect arguments
-    p = optparse.OptionParser(usage="%prog GALAXY_URL API_KEY EMAIL [PUBLIC_NAME]",
-                              description="Create a new user in the specified Galaxy "
+    p = optparse.OptionParser(usage="%prog options GALAXY_URL API_KEY EMAIL [PUBLIC_NAME]",
+                              description="Create new user(s) in the specified Galaxy "
                               "instance")
+    p.add_option('-p','--password',action='store',dest='passwd',default=None,
+                 help="specify password for new user account (otherwise program will "
+                 "prompt for password")
+    p.add_option('-c','--check',action='store_true',dest='check',default=False,
+                 help="check user details but don't try to create the new account")
     options,args = p.parse_args()
     if len(args) < 3 or len(args) > 4:
         p.error("Wrong arguments")
@@ -38,19 +43,25 @@ if __name__ == "__main__":
     ni = nebulizer.Nebulizer(galaxy_url,api_key)
     if not ni.check_new_user_info(email,name):
         sys.exit(1)
+    if options.check:
+        print "Email and username ok: not currently in use"
+        sys.exit(0)
 
     # Prompt for password
-    passwd = getpass.getpass("Enter password for new user: ")
+    passwd = options.passwd
+    if passwd is None:
+        passwd = getpass.getpass("Enter password for new user: ")
     if len(passwd) < 6:
         sys.stderr.write("Invalid password: must be 6 or more characters\n")
         sys.exit(1)
-    passwd2 = getpass.getpass("Confirm password: ")
-    if passwd2 != passwd:
-        sys.stderr.write("Passwords don't match\n")
-        sys.exit(1)
+    if options.passwd is None:
+        passwd2 = getpass.getpass("Confirm password: ")
+        if passwd2 != passwd:
+            sys.stderr.write("Passwords don't match\n")
+            sys.exit(1)
 
     # Create the new user
     if not ni.create_user(email,name,passwd):
         sys.exit(1)
-    print "Done"
+    print "Created new account for %s" % email
     
