b3c1e2077ef4db9d68ff15387802272483c6f0d6
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-02-19 14:30:25 +0000

    create_user.py: refactor internals and add option to create batch of users from template email.

--- create_user.py
+++ create_user.py
@@ -11,64 +11,199 @@ create_user.py
 
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
+
+def get_passwd():
+    """Prompt user for a password
+
+    Prompts user to enter and confirm a password. Raises an exception
+    if the password is deemed to be invalid (e.g. too short), or if
+    the password confirmation fails.
+
+    Returns:
+      Password string entered by the user.
+
+    """
+    passwd = getpass.getpass("Enter password for new user: ")
+    if len(passwd) < 6:
+        raise Exception("Invalid password: must be 6 or more characters")
+    passwd2 = getpass.getpass("Confirm password: ")
+    if passwd2 != passwd:
+        raise Exception("Passwords don't match")
+    return passwd
+
+def create_user(ni,email,name=None,passwd=None,only_check=False):
+    """Creates a new Galaxy user
+
+    Attempts to create a single user in a Galaxy instance with the
+    supplied credentials.
+
+    Arguments:
+      ni    : Nebulizer instance associated with a Galaxy instance
+      email : email address for the new user
+      name  : (optional) name to associate with the user. If
+        'None' then will be generated from the email address.
+      passwd: (optional) password for the new user. If 'None' then
+        the user will be prompted to supply a password.
+      only_check: if True then only run the checks, don't try to
+        make the user on the system.
+
+    Returns:
+      0 on success, 1 on failure.
+
+    """
+    # Check if user already exists
+    if not ni.check_new_user_info(email,name):
+        return 1
+    if only_check:
+        print "Email and username ok: not currently in use"
+        return 0
+    # Prompt for password
+    if passwd is None:
+        try:
+            passwd = get_passwd()
+        except Exception, ex:
+            sys.stderr.write("%s\n" % ex)
+            return 1
+    # Create the new user
+    if not ni.create_user(email,name,passwd):
+        return 1
+    print "Created new account for %s" % email
+    return 0
+
+def create_users_from_template(ni,template,start,end,passwd=None,
+                               only_check=False):
+    """Creates a batch of users in Galaxy, based on template email
+
+    Attempts to create a multiple users in a Galaxy instance, using
+    a template email address and a range of integer indices to
+    generate the names.
+
+    The 'template' email should contain a single hash character (i.e.
+    '#') within the leading name part of the address, for example:
+
+    student#@galaxy.ac.uk
+
+    'start' and 'end' together define a range of indices that will
+    replace the '#' to generate emails of the form:
+
+    student1@galaxy.ac.uk
+    student2@galaxy.ac.uk
+    ...etc...
+
+    Names will be generated automatically from the email addresses;
+    the same password will be used for all the new accounts.
+
+    Arguments:
+      ni       : Nebulizer instance associated with a Galaxy instance
+      template : template email address for the batch of new users
+      start    : initial integer index for user names
+      end      : final integer index for user names
+      passwd   : (optional) password for the new user. If 'None' then
+        the user will be prompted to supply a password.
+      only_check: if True then only run the checks, don't try to
+        make the users on the system.
+
+    Returns:
+      0 on success, 1 on failure.
+    
+    """
+    # Check template
+    name,domain = template.split('@')
+    if name.count('#') != 1 or domain.count('#') != 0:
+        sys.stderr.write("Incorrect email template format\n")
+        return 1
+    # Prompt for password
+    if passwd is None:
+        try:
+            passwd = get_passwd()
+        except Exception, ex:
+            sys.stderr.write("%s\n" % ex)
+            return 1
+    # Generate emails
+    emails = [template.replace('#',str(i)) for i in range(start,end)]
+    # Check that these are available
+    print "Checking availability"
+    for email in emails:
+        name = nebulizer.get_username_from_login(email)
+        ##print "%s, %s" % (email,name)
+        if not ni.check_new_user_info(email,name):
+            return 1
+    if only_check:
+        print "All emails and usernames ok: not currently in use"
+        return 0
+    # Make the accounts
+    for email in emails:
+        name = nebulizer.get_username_from_login(email)
+        print "Email : %s" % email
+        print "Name  : %s" % name
+        if not ni.create_user(email,name,passwd):
+            return 1
+        print "Created new account for %s" % email
+    return 0
 
 if __name__ == "__main__":
     # Collect arguments
-    p = optparse.OptionParser(usage="%prog options GALAXY_URL API_KEY EMAIL [PUBLIC_NAME]",
+    p = optparse.OptionParser(usage="\n%prog options GALAXY_URL API_KEY EMAIL [PUBLIC_NAME]",
                               version="%%prog %s" % __version__,
                               description="Create new user(s) in the specified Galaxy "
-                              "instance")
+                              "instance.")
     p.add_option('-p','--password',action='store',dest='passwd',default=None,
                  help="specify password for new user account (otherwise program will "
-                 "prompt for password")
+                 "prompt for password)")
     p.add_option('-c','--check',action='store_true',dest='check',default=False,
                  help="check user details but don't try to create the new account")
+    p.add_option('-t','--template',action='store_true',dest='template',default=False,
+                 help="indicates that EMAIL is actually a 'template' email address which "
+                 "includes a '#' symbol as a placeholder where an integer index should be "
+                 "substituted to make multiple accounts (e.g. 'student#@galaxy.ac.uk'). "
+                 "The --range option supplies the range of integer indices.")
+    p.add_option('-r','--range',action='store',dest='range',default=None,
+                 help="RANGE defines a set of integer indices to use when the --template "
+                 "option is specified. RANGE is of the form 'START[,END]' e.g. '1', '20,25'")
     options,args = p.parse_args()
     if len(args) < 3 or len(args) > 4:
         p.error("Wrong arguments")
     galaxy_url = args[0]
     api_key = args[1]
     email = args[2]
-
-    # Collect public name
-    try:
-        name = args[3]
-        if not nebulizer.check_username_format(name):
-            sys.stderr.write("Invalid name: must contain only lower-case letters, "
-                             "numbers and '-'\n")
-            sys.exit(1)
-    except IndexError:
-        # No public name supplied, make from email address
-        name = nebulizer.get_username_from_login(email)
-
-    print "Create new user in Galaxy instance at %s" % galaxy_url
-    print "Email : %s" % email
-    print "Name  : %s" % name
-
-    # Set up galaxy instance and check that email/name are not already in use
-    ni = nebulizer.Nebulizer(galaxy_url,api_key)
-    if not ni.check_new_user_info(email,name):
-        sys.exit(1)
-    if options.check:
-        print "Email and username ok: not currently in use"
-        sys.exit(0)
-
-    # Prompt for password
     passwd = options.passwd
-    if passwd is None:
-        passwd = getpass.getpass("Enter password for new user: ")
-    if len(passwd) < 6:
-        sys.stderr.write("Invalid password: must be 6 or more characters\n")
-        sys.exit(1)
-    if options.passwd is None:
-        passwd2 = getpass.getpass("Confirm password: ")
-        if passwd2 != passwd:
-            sys.stderr.write("Passwords don't match\n")
-            sys.exit(1)
 
-    # Create the new user
-    if not ni.create_user(email,name,passwd):
-        sys.exit(1)
-    print "Created new account for %s" % email
+    # Set up Nebulizer instance to interact with Galaxy
+    ni = nebulizer.Nebulizer(galaxy_url,api_key)
+
+    # Determine mode of operation
+    print "Create new users in Galaxy instance at %s" % galaxy_url
+    if options.template:
+        # Get the range of integer indices
+        if options.range is None:
+            sys.stderr.write("No range: use --range to specify start and end ID indices\n")
+            sys.exit(1)
+        start = int(options.range.split(',')[0])
+        try:
+            end = int(options.range.split(',')[1])
+        except IndexError:
+            end = start
+        # Create users
+        retval = create_users_from_template(ni,email,start,end,passwd,
+                                            only_check=options.check)
+    else:
+        # Collect public name
+        try:
+            name = args[3]
+            if not nebulizer.check_username_format(name):
+                sys.stderr.write("Invalid name: must contain only lower-case letters, "
+                                 "numbers and '-'\n")
+                sys.exit(1)
+        except IndexError:
+            # No public name supplied, make from email address
+            name = nebulizer.get_username_from_login(email)
+        # Create user
+        print "Email : %s" % email
+        print "Name  : %s" % name
+        retval = create_user(ni,email,name,passwd,
+                             only_check=options.check)
+
+    # Finished
+    sys.exit(retval)
     
