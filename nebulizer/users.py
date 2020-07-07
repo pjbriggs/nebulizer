73dccc41a7b6fdc92a968474a305bd02fdeef8b7
Peter Briggs <peter.briggs@manchester.ac.uk>
2020-07-07 08:15:40 +0100

    delete_user: in confirmation prompt, fallback to Python2 'raw_input' builtin if Python3 'input' builtin not found.

--- nebulizer/users.py
+++ nebulizer/users.py
@@ -562,7 +562,10 @@ def prompt_for_confirmation(question,default=None):
                         default)
     while True:
         sys.stdout.write(question + prompt)
-        choice = input().lower()
+        try:
+            choice = input().lower()
+        except NameError:
+            choice = raw_input().lower()
         if default is not None and choice == '':
             return responses[default.lower()]
         elif choice in responses:
